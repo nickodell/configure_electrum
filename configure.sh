062e3135b35493e426de5b20f651d8be9045df1d
nick <nick@host.frccnetlab.local>
2015-04-17 13:31:05 -0600

    Automatically generated commit

--- configure.sh
+++ configure.sh
@@ -2,8 +2,10 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 die() { echo "$@" 1>&2 ; exit 1; }
 
 adduser bitcoin --disabled-password || die
+echo 1
 apt-get install -y git make g++ python-leveldb libboost-all-dev \
                    libssl-dev libdb++-dev pkg-config || die
+echo 2
 # Fix path
 echo "PATH=\"\$HOME/bin:\$PATH\"" >> ~bitcoin/.login
 # If we create the file as root, the file might not be owned by bitcoin
--- copy_to_target.sh
+++ copy_to_target.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-ssh "$@" "apt-get install -y git && \
+git add .
+git commit -m "Automatically generated commit"
+git push
+ssh "$@" "rm -rf /root/configure_electrum; \
+          apt-get install -y git && \
           git clone https://github.com/nickodell/configure_electrum && \
           ./configure_electrum/configure.sh"
